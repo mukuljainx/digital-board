415a302479dee8efaf13ae3b278b8aedcb1dbe29
Mukul Jain <jainmukul1996@gmail.com>
2020-09-19 16:19:29 +0530

    feat: add simple whiteboard

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,65 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import * as React from "react";
 
-function App() {
+type Plots = Array<{ x: number; y: number }>;
+
+const App = () => {
+  const [drawing, setDrawing] = React.useState(false);
+  const points = React.useRef<Plots>([]);
+  const canvasRef = React.useRef<HTMLCanvasElement>(null);
+  const canvasContext = React.useRef<CanvasRenderingContext2D | null>(null);
+
+  const drawOnCanvas = (plots: Plots, color = "black") => {
+    const ctx = canvasContext.current!;
+    console.log("drawOnCanvas");
+    ctx.beginPath();
+    ctx.moveTo(plots[0].x, plots[0].y);
+
+    for (var i = 1; i < plots.length; i++) {
+      ctx.lineTo(plots[i].x, plots[i].y);
+    }
+    ctx.stroke();
+  };
+
+  const handleMouseMove = (reactEvent: React.MouseEvent) => {
+    const event = reactEvent.nativeEvent;
+    if (drawing) {
+      const x = event.offsetX;
+      const y = event.offsetY;
+
+      points.current.push({ x: x, y: y });
+      console.log(points.current);
+      drawOnCanvas(points.current);
+    }
+  };
+
+  const handleMouseDown = (event: any) => {
+    console.log(event);
+    setDrawing(true);
+  };
+
+  const handleMouseUp = () => {
+    points.current = [];
+    setDrawing(false);
+  };
+
+  React.useEffect(() => {
+    canvasContext.current = canvasRef.current!.getContext("2d");
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <canvas
+        ref={canvasRef}
+        id="drawCanvas"
+        width="800"
+        height="600"
+        style={{ border: "1px solid" }}
+        onMouseMove={handleMouseMove}
+        onMouseDown={handleMouseDown}
+        onMouseUp={handleMouseUp}
+      />
     </div>
   );
-}
+};
 
 export default App;

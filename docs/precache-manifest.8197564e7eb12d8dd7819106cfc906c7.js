4bd03b726574b552de5a92fa7384c4148a58d582
Mukul Jain <jainmukul1996@gmail.com>
2020-09-21 04:54:00 +0530

    chore: build app

---
+++ docs/precache-manifest.8197564e7eb12d8dd7819106cfc906c7.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "882b54384488fe0304a995beb9334793",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c51402e53a595e135d00",
+    "url": "/static/css/main.0d6cdd74.chunk.css"
+  },
+  {
+    "revision": "baf3746a7a3b5de8cfb8",
+    "url": "/static/js/2.8d1fe3a1.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.8d1fe3a1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c51402e53a595e135d00",
+    "url": "/static/js/main.fc1e880d.chunk.js"
+  },
+  {
+    "revision": "73635f645c271e35ff15",
+    "url": "/static/js/runtime-main.83c3e0c4.js"
+  },
+  {
+    "revision": "27b127d30bccd3bb2efd092d4309e9f6",
+    "url": "/static/media/brush.27b127d3.svg"
+  },
+  {
+    "revision": "da6472590083f29ce35387b098a966fd",
+    "url": "/static/media/eraser.da647259.svg"
+  },
+  {
+    "revision": "8d3a7b02f46a8aa674fd3166aae1238d",
+    "url": "/static/media/font.8d3a7b02.svg"
+  },
+  {
+    "revision": "66601412c680fe837f901bfb724a99d8",
+    "url": "/static/media/highlighter.66601412.svg"
+  }
+]);
